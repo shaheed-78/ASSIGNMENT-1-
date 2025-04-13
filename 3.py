8383ec59d5e0b8b5e2e7c5e29d1faf93f3aca7ac
shaheed-78 <shahidafridshekh@gmail.com>
2025-04-13 22:27:23 +0530

    Update 3.py

--- 3.py
+++ 3.py
@@ -1,6 +1,6 @@
 def calculatour ():
-    a1=float(input("Enter the first number: "))
-    a2=float(input("Enter the second number: "))
+    num1=float(input("Enter the first number: "))
+    num2=float(input("Enter the second number: "))
     add = num1 + num2
     subtract = num1 - num2
     multiply = num1 * num2
