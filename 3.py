9aac2b0224a8fd6636881f5f4b0c6141b2f17140
shaheed-78 <shahidafridshekh@gmail.com>
2025-04-13 22:05:41 +0530

    Add files via upload

---
+++ 3.py
@@ -0,0 +1,12 @@
+def calculatour ():
+    a1=float(input("Enter the first number: "))
+    a2=float(input("Enter the second number: "))
+    add = num1 + num2
+    subtract = num1 - num2
+    multiply = num1 * num2
+    divide = num1 / num2
+    print("Addition:", add)
+    print("Subtraction:", subtract)
+    print("Multiplication:", multiply)
+    print("Division:", divide)
+calculator()
---
+++ 4.py
@@ -0,0 +1,6 @@
+def get_full_name():
+   first_name = input("Enter your first name: ")
+   last_name = input("Enter your last name: ")
+   full_name = first_name + " " + last_name
+   print(f"Hello, {full_name}! It's nice to meet you.")
+get_full_name()
